85a1d508ff7a550a18e06d5602e141205a09dce9
jx <jxadmin@qq.com>
2024-01-10 15:27:54 +0800

    还原华为隐私协议

--- lib_common/src/main/java/com/mshy/VInterestSpeed/common/constant/NetBaseUrlConstant.kt
+++ lib_common/src/main/java/com/mshy/VInterestSpeed/common/constant/NetBaseUrlConstant.kt
@@ -31,39 +31,39 @@ object NetBaseUrlConstant {
             return field
         }
 
-//    const val VIP_URL = "/index/about/vip_agreement_qy.html"
-//    const val CONTRACT_URL = "/index/about/service.html"
-//    const val HELP_URL_BOY = "/index/about/help_boy.html"
-//    const val HELP_URL_GIRL = "/index/about/help_girl.html"
-//    const val HELP_URL_SPECIAL = "/index/about/help_audit.html"
-//    const val AGREEMENT_URL = "/tianyuan/about/agreement_qy.html" //用户协议
-//    const val RULE_URL = "/index/about/invite_help.html" //规则详情
+    const val VIP_URL = "/index/about/vip_agreement_qy.html"
+    const val CONTRACT_URL = "/index/about/service.html"
+    const val HELP_URL_BOY = "/index/about/help_boy.html"
+    const val HELP_URL_GIRL = "/index/about/help_girl.html"
+    const val HELP_URL_SPECIAL = "/index/about/help_audit.html"
+    const val AGREEMENT_URL = "/tianyuan/about/agreement_qy.html" //用户协议
+    const val RULE_URL = "/index/about/invite_help.html" //规则详情
+
+    const val USER_PRIVACY_URL = "/tianyuan/about/privacy_qy.html" //用户隐私协议
+
+    const val USER_permission_URL = "/tianyuan/about/permission_qy.html" //权限隐私
+
+    const val USER_thirdparty_sdk_URL = "/index/about/thirdparty_sdk_qy.html" //第三方sdk目录
+
+    const val RECHARGE_AGREEMENT = "/index/about/recharge_agreement_qy.html"    //充值协议
+    const val JUVENILE_PROTECTION = "/index/about/juvenile_protection_qy.html"    //未成年保护计划
+
+//    const val VIP_URL = "/index/about/vip_agreement_qy_hw.html"
+//    const val CONTRACT_URL = "/index/about/service_hw.html"
+//    const val HELP_URL_BOY = "/index/about/help_boy_hw.html"
+//    const val HELP_URL_GIRL = "/index/about/help_girl_hw.html"
+//    const val HELP_URL_SPECIAL = "/index/about/help_audit_hw.html"
+//    const val AGREEMENT_URL = "/tianyuan/about/agreement_qy_hw.html" //用户协议
+//    const val RULE_URL = "/index/about/invite_help_hw.html" //规则详情
 //
-//    const val USER_PRIVACY_URL = "/tianyuan/about/privacy_qy.html" //用户隐私协议
+//    const val USER_PRIVACY_URL = "/tianyuan/about/privacy_qy_hw.html" //用户隐私协议
 //
-//    const val USER_permission_URL = "/tianyuan/about/permission_qy.html" //权限隐私
+//    const val USER_permission_URL = "/tianyuan/about/permission_qy_hw.html" //权限隐私
 //
-//    const val USER_thirdparty_sdk_URL = "/index/about/thirdparty_sdk_qy.html" //第三方sdk目录
+//    const val USER_thirdparty_sdk_URL = "/index/about/thirdparty_sdk_qy_hw.html" //第三方sdk目录
 //
-//    const val RECHARGE_AGREEMENT = "/index/about/recharge_agreement_qy.html"    //充值协议
-//    const val JUVENILE_PROTECTION = "/index/about/juvenile_protection_qy.html"    //未成年保护计划
-
-    const val VIP_URL = "/index/about/vip_agreement_qy_hw.html"
-    const val CONTRACT_URL = "/index/about/service_hw.html"
-    const val HELP_URL_BOY = "/index/about/help_boy_hw.html"
-    const val HELP_URL_GIRL = "/index/about/help_girl_hw.html"
-    const val HELP_URL_SPECIAL = "/index/about/help_audit_hw.html"
-    const val AGREEMENT_URL = "/tianyuan/about/agreement_qy_hw.html" //用户协议
-    const val RULE_URL = "/index/about/invite_help_hw.html" //规则详情
-
-    const val USER_PRIVACY_URL = "/tianyuan/about/privacy_qy_hw.html" //用户隐私协议
-
-    const val USER_permission_URL = "/tianyuan/about/permission_qy_hw.html" //权限隐私
-
-    const val USER_thirdparty_sdk_URL = "/index/about/thirdparty_sdk_qy_hw.html" //第三方sdk目录
-
-    const val RECHARGE_AGREEMENT = "/index/about/recharge_agreement_qy_hw.html"    //充值协议
-    const val JUVENILE_PROTECTION = "/index/about/juvenile_protection_qy_hw.html"    //未成年保护计划
+//    const val RECHARGE_AGREEMENT = "/index/about/recharge_agreement_qy_hw.html"    //充值协议
+//    const val JUVENILE_PROTECTION = "/index/about/juvenile_protection_qy_hw.html"    //未成年保护计划
 
     val AGREEMENT_BASE_URL = "https://asset.whzhenban.top"
 }
