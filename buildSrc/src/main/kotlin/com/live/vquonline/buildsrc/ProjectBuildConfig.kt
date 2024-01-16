6cd13e2ae20678b54edb307d7601e7e8489ba9b1
jx <jxadmin@qq.com>
2024-01-16 14:40:29 +0800

    升级版本号

--- buildSrc/src/main/kotlin/com/live/vquonline/buildsrc/ProjectBuildConfig.kt
+++ buildSrc/src/main/kotlin/com/live/vquonline/buildsrc/ProjectBuildConfig.kt
@@ -12,8 +12,8 @@ object ProjectBuildConfig {
     const val applicationId = "com.shanhai.tianyuan"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 6
-    const val versionName = "1.0.5"
+    const val versionCode = 8
+    const val versionName = "1.0.7"
     const val isAppMode = false
 
     /**
